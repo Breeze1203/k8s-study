07d88992987489a6dd371c9ace465ca64fe34f0d
pengtao <3548297839@qq.com>
2026-06-18 14:08:35 +0800

    init environment

--- init_environment.sh
+++ init_environment.sh
@@ -4,7 +4,13 @@
 set -e
 
 echo "=========================================="
-echo " 1. 开始下载并安装 Minikube..."
+echo " 1. 开始下载并安装 docker..."
+echo "=========================================="
+curl -fsSL https://get.docker.com | bash -s docker
+sudo usermod -aG docker $USER && newgrp docker
+
+echo "=========================================="
+echo " 2. 开始下载并安装 Minikube..."
 echo "=========================================="
 curl -LO https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
@@ -12,16 +18,16 @@ rm minikube-linux-amd64
 minikube version
 
 echo "=========================================="
-echo " 2. 开始下载并安装 Kubectl..."
+echo " 3. 开始下载并安装 Kubectl..."
 echo "=========================================="
 # 自动获取最新稳定版版本号并下载
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 rm kubectl 
-kubectl version
+kubectl version --client
 
 echo "=========================================="
-echo " 3. 配置 Kubectl 自动补全..."
+echo " 4. 配置 Kubectl 自动补全..."
 echo "=========================================="
 # 更新源并安装 bash-completion（-y 自动确认）
 sudo apt-get update && sudo apt-get install -y bash-completion
@@ -31,7 +37,7 @@ if ! type _init_completion 2>/dev/null; then
     source /usr/share/bash-completion/bash_completion
 fi
 
-# 官方推荐：直接将补全脚本生成到系统补全目录下，免去修改 .bashrc 的麻烦
+# 直接将补全脚本生成到系统补全目录下，免去修改 .bashrc 的麻烦
 sudo mkdir -p /etc/bash_completion.d
 kubectl completion bash | sudo tee /etc/bash_completion.d/kubectl > /dev/null
 
