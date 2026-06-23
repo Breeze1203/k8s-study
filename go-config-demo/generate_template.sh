4c9c3f5ff30aeb20df6f8dcf7d1c0188d460f109
pengtao <3548297839@qq.com>
2026-06-23 11:37:46 +0800

    update file suffix

--- go-config-demo/generate_template.sh
+++ go-config-demo/generate_template.sh
@@ -1,14 +1,31 @@
+#!/bin/bash
+set -e
+
 # generate deployment template
-kubectl create deployment nginx --image=nginx:latest  --dry-run=client -o yaml > deployment.yaml
+kubectl create deployment nginx \
+  --image=nginx:latest \
+  --dry-run=client \
+  -o yaml > deployment.yaml
 
 # generate service template
-kubectl create service nodeport nginx --tcp=80:80 --dry-run=client -o yaml > service.yaml
+kubectl create service nodeport nginx \
+  --tcp=80:80 \
+  --dry-run=client \
+  -o yaml > service.yaml
 
 # generate configmap template
-kubectl create configmap crm-config --from-literal=DB_HOST=mysql  --from-literal=DB_PORT=3306 --dry-run=client -o yaml
+kubectl create configmap crm-config \
+  --from-literal=DB_HOST=mysql \
+  --from-literal=DB_PORT=3306 \
+  --dry-run=client \
+  -o yaml > configmap.yaml
 
-# generate configmap template
-kubectl create secret generic mysql-secret --from-literal=username=root --from-literal=password=123456 --dry-run=client  -o yaml
+# generate secret template
+kubectl create secret generic mysql-secret \
+  --from-literal=username=root \
+  --from-literal=password=123456 \
+  --dry-run=client \
+  -o yaml > secret.yaml
 
-# get existing resources
-kubectl get deployment nginx -o yaml
+echo "模板已生成："
+ls -l *.yaml
